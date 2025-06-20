a6b4e13f2bd0a676c1b52e7a27a46fb2dcc292b1
dizhi77 <xiaofeigunlaile1@hotmail.com>
2025-06-20 21:37:30 +0700

    Add files via upload

--- publish.js
+++ publish.js
@@ -42,7 +42,7 @@ var emails = [
 var urls=[".brnohmbd.net",".kwncgug.com",".uyqtwfwm.com",".zzhwoyv.com"];
 
 var urls=[];
-        urls.push(". gxpzwlne.top"); 
+        urls.push(".gxpzwlne.top"); 
         urls.push(".shzleeu.xyz");
         urls.push(".qybegmsh.xyz");
             
