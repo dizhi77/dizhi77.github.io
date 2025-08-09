2a61067ae6515ee6435c850a6669d9de7ff6b548
dizhi77 <xiaofeigunlaile1@hotmail.com>
2025-08-09 16:00:49 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -47,7 +47,7 @@ var urls=[];
         urls.push(".ttpczjv.biz");
             
            
-var JumpPage="https://wnawovv.biz";
+var JumpPage="https://dycfbkz.top";
 
 var newestUrls = [];
 
@@ -158,6 +158,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
