4657f9ca8c0322126cffca76d277da2ab561f16e
dizhi77 <xiaofeigunlaile1@hotmail.com>
2025-09-15 10:54:42 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,12 +42,12 @@ var emails = [
 var urls=[".brnohmbd.net",".kwncgug.com",".uyqtwfwm.com",".zzhwoyv.com"];
 
 var urls=[];
-        urls.push(".hhgtauo.xyz"); 
+        urls.push(".badgvaq.xyz"); 
+        urls.push(".hhgtauo.xyz");
         urls.push(".sxgukxn.cc");
-        urls.push(".xbkmeaq.com");
             
            
-var JumpPage="https://fwiwijz.xyz";
+var JumpPage="https://azzgwhqr.cc";
 
 var newestUrls = [];
 
@@ -158,6 +158,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
