51fbfc8fd9d3ac7daeb4e58a2e06daacea67fe11
dizhi77 <xiaofeigunlaile1@hotmail.com>
2025-12-13 05:58:20 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,12 +42,12 @@ var emails = [
 var urls=[".brnohmbd.net",".kwncgug.com",".uyqtwfwm.com",".zzhwoyv.com"];
 
 var urls=[];
-        urls.push(".drflpmi.cc"); 
+        urls.push(".hqmwcra.xyz"); 
+        urls.push(".drflpmi.cc");
         urls.push(".ydfgicjt.cc");
-        urls.push(".imuyhrxr.com");
             
            
-var JumpPage="https://dwiqryma.cc";
+var JumpPage="https://hsusssay.xyz";
 
 var newestUrls = [];
 
@@ -158,6 +158,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
