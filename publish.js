7fb2f962692b7d478cd7d56924bade9adee2227b
dizhi77 <xiaofeigunlaile1@hotmail.com>
2026-03-07 17:44:27 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,7 +42,7 @@ var emails = [
 var urls=[".brnohmbd.net",".kwncgug.com",".uyqtwfwm.com",".zzhwoyv.com"];
 
 var urls=[];
-        urls.push(".daiscqiv.xyz"); 
+        urls.push(".tejlowqw.xyz"); 
         urls.push(".bmqpasal.com");
         urls.push(".drgqqbia.xyz");
         urls.push(".pbxqhuot.cc");    
@@ -158,6 +158,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
