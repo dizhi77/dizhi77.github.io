2d3e92086b6d81031ca878c105b62aba081eb114
dizhi77 <xiaofeigunlaile1@hotmail.com>
2026-07-17 21:58:23 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -45,7 +45,7 @@ var urls=[];
         urls.push(".octaajify.cc"); 
         urls.push(".ehoavgydq.cc");
         urls.push(".ssyedmcj.com");
-        urls.push(".mffinjte.cc");    
+        urls.push(".gzomhzzao.cc");    
            
 var JumpPage="https:///jbrrmfju.cc";
 
