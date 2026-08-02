59f33100d3b997a7e4c4c7988b8865af2a414f04
dizhi77 <xiaofeigunlaile1@hotmail.com>
2026-08-02 21:27:21 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,7 +42,7 @@ var emails = [
 var urls=[".brnohmbd.net",".kwncgug.com",".uyqtwfwm.com",".zzhwoyv.com"];
 
 var urls=[];
-        urls.push(".ertlnefry.cc"); 
+        urls.push(".qorcwxeq.cc"); 
         urls.push(".ertlnefry.cc");
         urls.push(".trgmafmzq.com");
         urls.push(".kdixkybiv.cc");    
